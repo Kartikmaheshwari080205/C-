6a8768119b646697052566c7ddad7e56d20c27c1
Kartik Maheshwari <kartik080205@gmail.com>
2025-08-23 23:23:32 +0530

    23-08

--- divisibletriplets.cpp
+++ divisibletriplets.cpp
@@ -1,55 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int Factorial(int n)
+int DivisibleTriplets(int n, int k)
 {
-    int result = 1;
-    for(int i=1; i<=n; i++)
+    int mod0 = n / k;
+    int result = pow(mod0, 3);
+    if(k % 2 == 0)
     {
-        result *= i;
+        int modkby2 = mod0 + ((n % k) >= (k / 2));
+        result += pow(modkby2, 3);
     }
     return result;
-}
-
-int nCk(int n, int k)
-{
-    return Factorial(n) / (Factorial(k) * Factorial(n-k));
-}
-
-int DivisibleTriplets(int n, int k, vector<int>& A)
-{
-    int mod0 = 0;
-    for(int a : A)
-    {
-        if(a % k == 0)
-        {
-            mod0++;
-        }
-    }
-    int result = nCk(mod0, 3);
-    if(k % 2 == 1)
-    {
-        return result;
-    }
-    int modkby2 = 0;
-    for(int a : A)
-    {
-        if(a % k == k/2)
-        {
-            modkby2++;
-        }
-    }
-    return result + nCk(modkby2, 3);
 }
 
 int main()
 {
     int n, k;
     cin >> n >> k;
-    vector<int> A(n);
-    for(int i=0; i<n; i++)
-    {
-        cin >> A[i];
-    }
-    cout << DivisibleTriplets(n, k, A) << endl;
+    cout << DivisibleTriplets(n, k) << endl;
 }
