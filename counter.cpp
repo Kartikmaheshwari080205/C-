c751295b373e458a7d5e44ee82bf95402936af6f
Kartik Maheshwari <kartik080205@gmail.com>
2025-09-17 21:14:56 +0530

    17-09

---
+++ counter.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool CanServe(int n, int m, vector<int>& A, int time)
+{
+    int count = 0;
+    for(int i=0; i<n; i++)
+    {
+        count += (time / A[i]);
+    }
+    return count >= m;
+}
+
+int MintimeToProcess(int n, int m, vector<int>& A)
+{
+    sort(A.begin(), A.end());
+    int low = 0, high = A[0] * m;
+    int result = high;
+    while(low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if(CanServe(n, m, A, mid))
+        {
+            high = mid - 1;
+            result = min(result, mid);
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> A(n);
+    for(int i=0; i<n; i++)
+    {
+        cin >> A[i];
+    }
+    cout << MintimeToProcess(n, m, A) << endl;
+}
