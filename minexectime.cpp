6436b0d7c780972b668ea3b351c07ecc27dc16f7
Kartik Maheshwari <kartik080205@gmail.com>
2025-10-15 12:26:57 +0530

    15-10

---
+++ minexectime.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int MinExecutionTime(int n, vector<char>& A, int k)
+{
+    unordered_map<char, int> frequency;
+    unordered_map<char, int> index;
+    for(char a : A)
+    {
+        frequency[a]++;
+        if(!index.count(a))
+        {
+            index[a] = -k;
+        }
+    }
+    int current = 1;
+    while(!frequency.empty())
+    {
+        vector<pair<int, char>> candidate;
+        for(auto& [c, count] : frequency)
+        {
+            if(current - index[c] > k)
+            {
+                candidate.push_back({index[c], c});
+            }
+        }
+        if(candidate.empty())
+        {
+            cout << "null ";
+            current++;
+            continue;
+        }
+        else
+        {
+            sort(candidate.begin(), candidate.end());
+            char toexecute = candidate[0].second;
+            frequency[toexecute]--;
+            index[toexecute] = current;
+            if(frequency[toexecute] == 0)
+            {
+                frequency.erase(toexecute);
+            }
+            cout << toexecute << " ";
+            current++;
+        }
+    }
+    cout << endl;
+    return current - 1;
+}
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<char> A(n);
+    for(int i=0; i<n; i++)
+    {
+        cin >> A[i];
+    }
+    
+    cout << MinExecutionTime(n, A, k) << endl;
+}
